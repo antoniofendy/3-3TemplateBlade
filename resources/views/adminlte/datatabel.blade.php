29c169836fb2a633af6f0b7819e161dcae3e1be9
antoniofendy <layfenfen@gmail.com>
2020-07-01 19:20:13 +0700

    Perubahan responsive

--- resources/views/adminlte/datatabel.blade.php
+++ resources/views/adminlte/datatabel.blade.php
@@ -447,10 +447,23 @@
 <script src="{{asset('adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
 <script src="{{asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
 <link rel="stylesheet" href="{{asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<script src="{{asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
 <script>
     $(function () {
-        $("#example1").DataTable();
+        $("#example1").DataTable({
+        "responsive": true,
+        "autoWidth": false,
+        });
+        $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+        });
     });
-    let a = ['Hello', 'world'];
 </script>
 @endpush
