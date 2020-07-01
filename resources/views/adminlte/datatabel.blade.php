5dfc9f1f43bc49edcdd87a87a7a5eebd72fa9d7f
antoniofendy <layfenfen@gmail.com>
2020-07-01 19:04:33 +0700

    Update CSS pada data-tables

--- resources/views/adminlte/datatabel.blade.php
+++ resources/views/adminlte/datatabel.blade.php
@@ -446,6 +446,7 @@
 @push('script')
 <script src="{{asset('adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
 <script src="{{asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
+<link rel="stylesheet" href="{{asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
 <script>
     $(function () {
         $("#example1").DataTable();
